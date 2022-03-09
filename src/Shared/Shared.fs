b89edbd5689194bd9be2013b8327d0441d74023f
gustav_asu4team <pogorelov-alserg@yandex.ru>
2022-03-09 12:23:36 +0300

    move Shared code from SAFE-TodoList

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -19,3 +19,46 @@ module Route =
 type ITodosApi =
     { getTodos: unit -> Async<TodoOld list>
       addTodo: TodoOld -> Async<TodoOld> }
+
+// SAFE-TodoList
+type Counter = int
+type TodoId = int
+
+[<CLIMutable>]
+type Todo = {
+  Id: TodoId // auto-incremented
+  Description: string
+  Completed: bool
+  DateAdded: DateTime
+}
+
+let defaultTodo() =
+  { Id = 0 // Id is auto-incremented because of the CLIMutable attribute
+    Description = ""
+    Completed = false
+    DateAdded = DateTime.Now }
+
+type TodoError =
+  | TodoDoesNotExist
+  | InsertNotSuccessful
+  | UpdateNotSuccesful
+  | DeleteNotSuccesful
+
+type DeleteResult =
+  | Deleted
+  | DeleteError of TodoError
+
+type UpdateResult =
+  | Updated
+  | UpdateError of TodoError
+
+type Description = Description of string
+
+/// A type that specifies the communication protocol for client and server
+/// Every record field must have the type : 'a -> Async<'b> where 'a can also be `unit`
+/// Add more such fields, implement them on the server and they be directly available on client
+type ITodoProtocol =
+  { allTodos : unit -> Async<List<Todo>>
+    addTodo : Description -> Async<Option<Todo>>
+    toggleCompleted : TodoId -> Async<UpdateResult>
+    deleteTodo : TodoId -> Async<DeleteResult>  }
