992a5fed4621b061bd41dcadaa1f939e2d10759e
gustav_asu4team <pogorelov-alserg@yandex.ru>
2022-03-09 13:57:22 +0300

    remove old code

--- src/Shared/Shared.fs
+++ src/Shared/Shared.fs
@@ -2,25 +2,10 @@ module Shared
 
 open System
 
-type TodoOld = { Id: Guid; Description: string }
-
-module Todo =
-    let isValid (description: string) =
-        String.IsNullOrWhiteSpace description |> not
-
-    let create (description: string) =
-        { Id = Guid.NewGuid()
-          Description = description }
-
 module Route =
     let builder typeName methodName =
         sprintf "/api/%s/%s" typeName methodName
 
-type ITodosApi =
-    { getTodos: unit -> Async<TodoOld list>
-      addTodo: TodoOld -> Async<TodoOld> }
-
-// SAFE-TodoList
 type Counter = int
 type TodoId = int
 
